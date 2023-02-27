43fbc693270e1d1f7c04405117632adf0ee8719a
hoanganh <nguyenhoanganh.itc96@gmail.com>
2023-02-27 15:08:40 +0700

    test husky, lint-staged

--- db/fakeDate/users.ts
+++ db/fakeDate/users.ts
@@ -1,5 +1,5 @@
-import { faker } from "@faker-js/faker";
-import { Condition, Repository } from "typeorm";
-import { UsersEntity, UserType } from "../entities";
-import "colors";
-import { get } from "node-emoji";
+// import { faker } from "@faker-js/faker";
+// import { Condition, Repository } from "typeorm";
+// import { UsersEntity, UserType } from "../entities";
+// import "colors";
+// import { get } from "node-emoji";
